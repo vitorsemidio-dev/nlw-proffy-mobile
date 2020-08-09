c1115804192e766ccfc2e7fccd19ebf2f6e2474c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-08 23:50:09 -0300

    Page header estrutura

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/icons/logo.png';
 
 import styles from './styles';
 
 function PageHeader() {
-  return <View />
+  function handleGoBack() {
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Text>
+            <Image source={backIcon} resizeMode="contain" />
+          </Text>
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+    </View>
+  )
 }
 
 export default PageHeader;
