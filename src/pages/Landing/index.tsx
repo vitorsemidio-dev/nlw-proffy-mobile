eca490bba9b4a7d706c3a2c1f4b646ab5038994c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-08 21:53:32 -0300

    Criaçao componente landing

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Landing() {
+  return (
+    <>
+      <Text>
+        Hello World
+      </Text>
+    </>
+  )
+}
+
+export default Landing;
