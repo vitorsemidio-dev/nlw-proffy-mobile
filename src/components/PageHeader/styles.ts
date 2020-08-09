2c2f9fd47e675df53a88e49589cfec6b86ba0a9a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-09 00:00:15 -0300

    FInalizando estilo header

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -5,8 +5,21 @@ const styles = StyleSheet.create({
     padding: 40,
     backgroundColor: '#8257e5',
   },
-  topBar: {},
-  title: {},
+
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  title: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#fff',
+    fontSize: 24,
+    lineHeight: 32,
+    maxWidth: 160,
+    marginVertical: 40,
+  },
 });
 
 export default styles;
