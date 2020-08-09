7406e3026bdc1e20690312fd2b89809d520c5318
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-08 22:53:58 -0300

    Navegação para give classes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png';
@@ -9,6 +10,11 @@ import HeartIcon from '../../assets/images/icons/heart.png';
 import styles from './styles';
 
 function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -28,7 +34,9 @@ function Landing() {
           </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <TouchableOpacity
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}>
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>
             Dar aulas
