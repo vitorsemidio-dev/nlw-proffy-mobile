54011cb8499076ab3c82a9da9c382542a73c9f87
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-08 22:56:16 -0300

    RectButton

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png';
@@ -27,21 +28,21 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>
             Estudar
           </Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity
+        <RectButton
           onPress={handleNavigateToGiveClassesPage}
           style={[styles.button, styles.buttonSecondary]}>
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>
             Dar aulas
           </Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.connections}>
