1b8f0a20eab9eb41675c1d1b86fab7a8769e2a4d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-08-08 22:08:30 -0300

    Estilos textos

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -8,6 +8,13 @@ function Landing() {
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>
+          O que deseja fazer?
+        </Text>
+      </Text>
     </View>
   )
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -7,10 +7,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 40,
   },
+
   banner: {
     width: '100%',
     resizeMode: 'contain',
-  }
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontWeight: 'bold',
+  },
 });
 
 export default styles;
